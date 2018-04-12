f3c09e59db5d6fb84b67f3f1d28564be6470187e
Matt Aitken <matt@mattaitken.com>
2018-04-12 18:09:08 +0100

    Added a CaptureScreenshotBehaviour to do screenshots whilst in the edito

---
+++ CaptureScreenshotBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace TinyMatter.CardClash.Core {
+    
+    public class CaptureScreenshotBehaviour : MonoBehaviour {
+
+        [SerializeField] private string path = "~/Desktop/";
+
+        [Button("Take screenshot")]
+        private void TakeScreenshot() {
+
+            var currentDateTime = DateTime.Now.ToString("u");
+            
+            ScreenCapture.CaptureScreenshot($"{path}{Application.productName}-{currentDateTime}.png");
+        }
+        
+    }
+    
+}
