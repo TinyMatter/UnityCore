0ea01d41c57125f1740ed0c7f96a80e6b2c9295a
Matt Aitken <matt@mattaitken.com>
2018-04-12 10:58:50 +0100

    Added a Color extensions so you can copy a color with alpha, very common operation

---
+++ Extensions/ColorExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace TinyMatter.Extensions {
+    
+    public static class ColorExtensions {
+
+        public static Color CopyWithAlpha(this Color color, float alpha) {
+            color.a = alpha;
+            return color;
+        }
+        
+    }
+}
