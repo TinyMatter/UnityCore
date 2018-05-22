bdba186436728dcdac199ab997ce0b2bf017fcf0
Matt Aitken <matt@mattaitken.com>
2018-05-22 17:03:18 +0100

    Added PressGestureRecognizer: presses are valid if they don’t move far, no time limit

---
+++ Gestures/PressGestureHandler.cs
@@ -0,0 +1,54 @@
+﻿using DigitalRubyShared;
+using TinyMatter.CardClash.Gameplay;
+using TinyMatter.Core.Gestures;
+using UnityEngine;
+
+namespace TinyMatter.Core.Gestures {
+    
+    public class PressGestureHandler : GestureHandler {
+        
+        [SerializeField] private int minimumTouches = 1;
+        [SerializeField] private float maximumMovementDistance = 0.35f;
+        
+        [SerializeField] private GestureHandler[] requiredToFail;
+        
+        private PressGestureRecognizer gesture = new PressGestureRecognizer();
+
+        protected override GestureRecognizer GetRecognizer() {
+            return gesture;
+        }
+        
+        private void Start() {
+            //add gesture
+            gesture = new PressGestureRecognizer();
+            FingersScript.Instance.AddGesture(gesture);
+            
+            gesture.AllowSimultaneousExecutionWithAllGestures();
+            
+            gesture.MinimumNumberOfTouchesToTrack = minimumTouches;
+            gesture.ThresholdUnits = maximumMovementDistance;
+                        
+            gesture.StateUpdated += PressGesture_StateUpdated;
+
+            foreach (var gestureHandler in requiredToFail) {
+                RequireGestureHandlerToFail(gestureHandler);
+            }
+        }
+
+        private void OnDestroy() {
+            gesture.StateUpdated -= PressGesture_StateUpdated;
+        }
+
+        private void PressGesture_StateUpdated(GestureRecognizer gestureRecognizer) {
+            var screenTouchPosition = new Vector3(gestureRecognizer.FocusX, gestureRecognizer.FocusY, 0f);
+            var ray = unityService.mainCamera.ScreenPointToRay(screenTouchPosition);
+    
+            RaycastHit hit;
+            
+            Physics.Raycast(ray, out hit);
+
+            hitHandler?.Invoke(hit, gestureRecognizer.State);
+        }
+        
+    }
+}
---
+++ Gestures/PressGestureRecognizer.cs
@@ -0,0 +1,68 @@
+﻿using DigitalRubyShared;
+
+namespace TinyMatter.Core.Gestures {
+
+    /// <summary>
+    /// A press that can be an instant tap or a long press, as long as the touch doesn't move more than the threshold 
+    /// </summary>
+    public class PressGestureRecognizer : GestureRecognizer {
+
+        /// <summary>
+        /// How many units away the long press can move before failing. After the long press begins,
+        /// it is allowed to move any distance and stay executing. Default is 0.35.
+        /// </summary>
+        /// <value>The threshold in units</value>
+        public float ThresholdUnits { get; set; }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public PressGestureRecognizer() {
+            ThresholdUnits = 0.35f;
+        }
+
+        protected override void TouchesBegan(System.Collections.Generic.IEnumerable<GestureTouch> touches) {
+            CalculateFocus(CurrentTrackedTouches);
+
+            if (TrackedTouchCountIsWithinRange) {
+                SetState(GestureRecognizerState.Began);
+            }
+        }
+
+        protected override void TouchesMoved() {
+            CalculateFocus(CurrentTrackedTouches);
+
+            if (State == GestureRecognizerState.Failed) {
+                return;
+            }
+
+            if (!TrackedTouchCountIsWithinRange) {
+                SetState(GestureRecognizerState.Failed);
+                return;
+            }
+
+            if (State == GestureRecognizerState.Began) {
+                SetState(GestureRecognizerState.Executing);
+            }
+
+            //check distance
+            var distance = Distance(DistanceX, DistanceY);
+            if (distance > ThresholdUnits) {
+                SetState(GestureRecognizerState.Failed);
+            }
+
+        }
+
+        protected override void TouchesEnded() {
+            if (State == GestureRecognizerState.Began || State == GestureRecognizerState.Executing) {
+                CalculateFocus(CurrentTrackedTouches);
+                SetState(GestureRecognizerState.Ended);
+            }
+            else {
+                SetState(GestureRecognizerState.Failed);
+            }
+        }
+        
+    }
+}
