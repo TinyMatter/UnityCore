b0663f3465238d1076281edecca88ecb1e752a59
Matt Aitken <matt@mattaitken.com>
2018-05-23 17:17:17 +0100

    Added SoundEffectEditor

---
+++ Sound/Editor/SoundEffectEditor.cs
@@ -0,0 +1,22 @@
+﻿using TinyMatter.Core.Sound;
+using UnityEditor;
+using UnityEngine;
+
+namespace TinyMatter.CardClash.Game {
+
+    [CustomEditor(typeof(SoundEffect))]
+    public class SoundEffectEditor : UnityEditor.Editor {
+        public override void OnInspectorGUI() {
+            base.OnInspectorGUI();
+
+            GUI.enabled = Application.isPlaying;
+
+            var e = target as SoundEffect;
+
+            if (GUILayout.Button("Play")) {
+                if (e != null) e.Play();
+            }
+        }
+    }
+
+}
