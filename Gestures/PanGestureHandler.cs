0943e2272be3dcd488cab403b751298c3b0eabea
Eric Allam <eallam@icloud.com>
2018-08-30 14:01:46 +0100

    Added support for drawing the gesture line in the scene

--- Gestures/PanGestureHandler.cs
+++ Gestures/PanGestureHandler.cs
@@ -1,4 +1,6 @@
-﻿using DigitalRubyShared;
+﻿using System;
+using System.Collections.Generic;
+using DigitalRubyShared;
 using UnityEngine;
 
 namespace TinyMatter.Core.Gestures {
@@ -25,14 +27,41 @@ namespace TinyMatter.Core.Gestures {
             gesture.StateUpdated -= PanGesture_StateUpdated;
         }
 
+        [NonSerialized]
+        public List<Vector3> registeredPoints = new List<Vector3>();
+
         private void PanGesture_StateUpdated(GestureRecognizer gestureRecognizer) {
             //get position in world space of touch
-            var screenTouchPosition = new Vector3(gestureRecognizer.FocusX, gestureRecognizer.FocusY, 0f);
+            var screenTouchPosition = new Vector3(gestureRecognizer.FocusX, gestureRecognizer.FocusY, 1f);
             var ray = unityService.mainCamera.ScreenPointToRay(screenTouchPosition);
             
             RaycastHit hit;
 
             Physics.Raycast(ray, out hit);
+
+            #if UNITY_EDITOR
+                if (hit.collider != null) {
+                    switch (gestureRecognizer.State) {
+                        case GestureRecognizerState.Possible:
+                            break;
+                        case GestureRecognizerState.Began:
+                        case GestureRecognizerState.Executing:
+                            registeredPoints.Add(hit.point);
+                            break;
+                        case GestureRecognizerState.Ended:
+                            registeredPoints.Clear();
+                            break;
+                        case GestureRecognizerState.EndPending:
+                            break;
+                        case GestureRecognizerState.Failed:
+                            registeredPoints.Clear();
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
+            #endif
+
 
             hitHandler?.Invoke(hit, gestureRecognizer.State);
         }
