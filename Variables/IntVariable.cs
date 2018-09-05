d21d6d821277ecddac38172f73a5a894bef8f680
Eric Allam <eallam@icloud.com>
2018-09-05 15:39:14 +0100

    Add persistence to IntVariables

--- Variables/IntVariable.cs
+++ Variables/IntVariable.cs
@@ -1,9 +1,24 @@
-﻿using UnityEngine;
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
 
 namespace TinyMatter.Core {
     
     [CreateAssetMenu(menuName = "Variables/Int")]
     public class IntVariable : BaseVariable<int> {
+        [SerializeField] public bool persistValue;
+
+        [Button]
+        [ShowIf("persistValue")]
+        public void ResetPersistentValue() {
+            PlayerPrefs.DeleteKey(name);
+        }
+        
+        protected override void ValueChanged() {
+            if (persistValue) {
+                PlayerPrefs.SetInt(name, Value);
+            }
+        }
+        
         public void Increment(int amount = 1) {
             SetValue(Value + amount);
         }
