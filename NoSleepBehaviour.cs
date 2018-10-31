a005ff7e1d1c0fc3ec6478bd8a56db05adfe2fa8
Matthew Aitken <matt@Matts-MacBook-Pro.local>
2018-10-31 17:49:46 +0000

    Prevent sleep behaviour now has a tickbox to persist between scene loads

--- NoSleepBehaviour.cs
+++ NoSleepBehaviour.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 namespace TinyMatter.GetFact.Game {
     public class NoSleepBehaviour : MonoBehaviour {
+        [SerializeField] private bool persist = true;
+        
         private void Awake() {
             Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+            if (persist) {
+                DontDestroyOnLoad(gameObject);
+            }
         }
     }
 }
